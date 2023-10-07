6ae842895e9284ed7889398201fa88e90a53f63a
zhuochenglee <1151246887@qq.com>
2023-10-07 15:17:19 +0800

    添加了时间显示，修复了ssim

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from data_util import ssim
 import network
 import argparse
 import os
+import time
 
 parser = argparse.ArgumentParser()
 
@@ -53,6 +54,7 @@ optimizer = torch.optim.Adam(net.parameters(), 0.01, betas=(0.9, 0.999))
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.4)
 best_pth = float('inf')
 
+start_time = time.time()
 for epoch in range(LOAD_EPOCHS, NUM_EPOCHS + 1):
     train_bar = tqdm(train_dataloader)
     # print(train_bar)
@@ -75,17 +77,17 @@ for epoch in range(LOAD_EPOCHS, NUM_EPOCHS + 1):
         fake_img_np = fake_img.cpu().detach().numpy()
         target_np = target.cpu().detach().numpy()
         # print(numpy.asarray(fake_img_np.shape) - 7)\
-        fake_img_np = np.squeeze(fake_img_np, axis=1)
-        target_np = np.squeeze(target_np, axis=1)
+        # fake_img_np = np.squeeze(fake_img_np, axis=1)
+        # target_np = np.squeeze(target_np, axis=1)
 
-        structural_similarity = ssim(fake_img_np, target_np)
-        print(structural_similarity)
+        # structural_similarity = ssim(fake_img_np, target_np)
+        # print(structural_similarity)
         g_loss = loss_fn(fake_img, target)
         g_loss.backward()
         optimizer.step()
 
         running_res['g_loss'] += g_loss.item() * batch_size
-        running_res['ssim'] += structural_similarity
+        # running_res['ssim'] += structural_similarity
 
         train_bar.set_description(desc='[%d] Loss_G: %.7f lr: %.7f' % (
             epoch, running_res['g_loss'] / running_res['batch_size'], optimizer.param_groups[0]['lr']))
@@ -97,12 +99,14 @@ for epoch in range(LOAD_EPOCHS, NUM_EPOCHS + 1):
             if current_loss < best_pth:
                 best_pth = current_loss
                 torch.save(net.state_dict(),
-                           save_dir + '/A_BEST')
+                           save_dir + '/A_BEST.pth')
             else:
                 torch.save(net.state_dict(), save_dir + '/net_epoch_%d_%6f.pth' % (
                     epoch, current_loss))
 
         scheduler.step()
     # ssim的最大值为 1.0
-    avg_ssim = running_res['ssim'] / batch_size
-    print(f'avg_ssim:{avg_ssim}')
+    # avg_ssim = running_res['ssim'] / batch_size
+    # print(f'avg_ssim:{avg_ssim}')
+total_time = time.time() - start_time
+print(f'total time:{total_time}')
