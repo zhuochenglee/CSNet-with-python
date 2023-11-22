bea8d4317edff4ed62ebb3f9e7a7ff394ad09dc1
zhuochenglee <1151246887@qq.com>
2023-11-22 13:36:06 +0800

    新增了网络代码，准备测试

--- test_code.py
+++ test_code.py
@@ -47,47 +47,71 @@ class ResidualBlock(nn.Module):
         return x + y
 
 
+class RB2O(nn.Module):
+    def __init__(self, cs, init_recon, blocksize=96, subrate=0.1):
+        super(RB2O, self).__init__()
+        self.cs = cs
+        self.init_recon = init_recon
+        self.samping = nn.Conv2d(1, int(numpy.round(blocksize * blocksize * subrate)), 3,
+                                       padding=0)  # 对初始重构图像进行采样提取特征，得到观测值
+        self.conv1 = nn.Conv2d(int(numpy.round(blocksize * blocksize * subrate)), blocksize * blocksize,
+                                     blocksize, padding=0, stride=blocksize)  # 通过初始重建图像的观测值生成预测图像
+        self.samping_2 = nn.Conv2d(1, int(numpy.round(blocksize * blocksize * subrate)), 3,
+                                         padding=0)  # 得到预测图像的观测值
+        self.init = nn.Conv2d(int(numpy.round(blocksize * blocksize * subrate)), blocksize * blocksize,
+                                       blocksize, padding=0)  # 通过观测值残差重新生成一张图像
+
+    def forward(self):
+        sampe_cs = self.samping(self.init_recon)
+        prediction_img = self.conv1(sampe_cs)
+        prediction_img_cs = self.samping_2(prediction_img)
+        residual_cs = self.cs - prediction_img_cs
+        init_res = self.init(residual_cs)
+        result = init_res + self.init_recon
+        return result
+
+
 # CSNet
 class CSNet(nn.Module):
-    def __init__(self, blocksize=32, subrate=0.1):
+    def __init__(self, blocksize=96, subrate=0.1):
         super(CSNet, self).__init__()
         self.blocksize = blocksize
-        self.samping = SeparableConv2d(1, int(numpy.round(blocksize * blocksize * subrate)), blocksize,
+        self.samping = nn.Conv2d(1, int(numpy.round(blocksize * blocksize * subrate)), blocksize,
                                        stride=blocksize, padding=0,
                                        bias=False)
         self.deconv = nn.ConvTranspose2d(int(numpy.round(blocksize * blocksize * subrate)), blocksize * blocksize,
-                                         blocksize, padding=0, stride=blocksize)
+                                         3, padding=0, stride=blocksize)
         # self.init_conv = nn.Conv2d(int(numpy.round(blocksize * blocksize * subrate)),
         #                            blocksize * blocksize, 1, stride=1, padding=0)
         self.c1 = nn.Sequential(
-            SeparableConv2d(in_channels=blocksize * blocksize, out_channels=32, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=blocksize * blocksize, out_channels=32, kernel_size=3, stride=1),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 3, 1),
+            nn.Conv2d(32, 32, 3, 1),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 3, 1),
+            nn.Conv2d(32, 32, 3, 1),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 3, 1),
+            nn.Conv2d(32, 32, 3, 1),
             nn.ReLU(),
         )
         self.c2_d1 = nn.Sequential(
-            SeparableConv2d(in_channels=blocksize * blocksize, out_channels=32, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=blocksize * blocksize, out_channels=32, kernel_size=3, stride=1),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 3, 1),
+            nn.Conv2d(32, 32, 3, 1),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 3, 1),
+            nn.Conv2d(32, 32, 3, 1),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 3, 1),
+            nn.Conv2d(32, 32, 3, 1),
             nn.ReLU(),
         )
         self.c2_d2 = nn.Sequential(
             nn.Conv2d(in_channels=numpy.round(blocksize * blocksize), out_channels=32, kernel_size=3, stride=1,
                       dilation=2),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 5, 1),
+            nn.Conv2d(32, 32, 5, 1),
             nn.ReLU(),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, dilation=2),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 5, 1),
+            nn.Conv2d(32, 32, 5, 1),
             nn.ReLU(),
         )
         self.c3_d1 = nn.Sequential(
@@ -113,11 +137,11 @@ class CSNet(nn.Module):
         self.c3_d3 = nn.Sequential(
             nn.Conv2d(in_channels=blocksize * blocksize, out_channels=32, kernel_size=3, stride=1, dilation=3),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 7, 1),
+            nn.Conv2d(32, 32, 7, 1),
             nn.ReLU(),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, dilation=3),
             nn.ReLU(),
-            SeparableConv2d(32, 32, 7, 1),
+            nn.Conv2d(32, 32, 7, 1),
             nn.ReLU(),
         )
         self.block1 = nn.Sequential(
@@ -136,18 +160,13 @@ class CSNet(nn.Module):
         print(cs.shape)"""
         # 初始重建
         x = self.deconv(cs)
-        # out = x.cpu().detach().numpy()
-        # channel_0 = out[0, 3, :, :]
-        # plt.imshow(channel_0, cmap='gray')
-        # plt.axis('off')
-        # plt.show()
-        # x = My_Reshape_Adap(x, self.blocksize)
+        feature = RB2O(cs=cs, init_recon=x)
         # 深度重建
         c1_out = self.c1(x)
-        c2_d1_out = c1_out
+        c2_d1_out = self.c2_d1(x)
         c2_d2_out = self.c2_d2(x)
-        c3_d1_out = c1_out
-        c3_d2_out = c2_d2_out
+        c3_d1_out = self.c3_d1(x)
+        c3_d2_out = self.c3_d2(x)
         c3_d3_out = self.c3_d3(x)
         c2_d2_out = F.interpolate(c2_d2_out, size=(c2_d1_out.size(2), c2_d1_out.size(3)), mode='bilinear',
                                   align_corners=False)
